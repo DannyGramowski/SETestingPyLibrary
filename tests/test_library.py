47ca9ad7fa9b23521c77aada21e9da984e9ccacb
Matthew Oley <64291507+mroley1@users.noreply.github.com>
2025-09-23 10:22:24 -0400

    write tests for API methods

---
+++ tests/test_library.py
@@ -0,0 +1,95 @@
+import unittest
+from unittest.mock import Mock
+from library import library
+import json
+
+class TestLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.lib = library.Library()
+        with open('tests_data/ebooks.txt', 'r') as f:
+            self.ebooks_data = json.loads(f.read())
+        with open('tests_data/book_data.txt', 'r') as f:
+            self.book_data = json.loads(f.read())
+
+    def test_is_ebook_true(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.ebooks_data)
+        tst_value = "learning python"
+        
+        result = self.lib.is_ebook(tst_value)
+        
+        self.lib.api.get_ebooks.assert_called_once_with(tst_value)
+        self.assertTrue(result)
+        
+    def test_is_ebook_false(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.ebooks_data)
+        tst_value = "eating python"
+        
+        result = self.lib.is_ebook(tst_value)
+        
+        self.lib.api.get_ebooks.assert_called_once_with(tst_value)
+        self.assertFalse(result)
+        
+    def test_is_ebook_uppercase_true(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.ebooks_data)
+        tst_value = "LEARNING PyThOn"
+        
+        result = self.lib.is_ebook(tst_value)
+        
+        self.lib.api.get_ebooks.assert_called_once_with(tst_value)
+        self.assertTrue(result)
+
+    def test_get_ebooks_count(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.ebooks_data)
+        tst_value = "learning python"
+        
+        actual = self.lib.get_ebooks_count(tst_value)
+        expected = 8
+        
+        self.lib.api.get_ebooks.assert_called_once_with(tst_value)
+        self.assertEqual(actual, expected)
+
+    def test_is_book_by_author(self):
+        book_data = ["book1", "book2"]
+        self.lib.api.books_by_author = Mock(return_value=book_data)
+        tst_value_author = "author name"
+        tst_value_book = "book2"
+        
+        result = self.lib.is_book_by_author(tst_value_author, tst_value_book)
+        
+        self.lib.api.books_by_author.assert_called_once_with(tst_value_author)
+        self.assertTrue(result)
+
+    def test_is_book_by_author_false(self):
+        book_data = ["book1", "book2"]
+        self.lib.api.books_by_author = Mock(return_value=book_data)
+        tst_value_author = "author name"
+        tst_value_book = "book3"
+        
+        result = self.lib.is_book_by_author(tst_value_author, tst_value_book)
+        
+        self.lib.api.books_by_author.assert_called_once_with(tst_value_author)
+        self.assertFalse(result)
+
+    def test_is_book_by_author_not_found(self):
+        book_data = []
+        self.lib.api.books_by_author = Mock(return_value=book_data)
+        tst_value_author = "author name"
+        tst_value_book = "book2"
+        
+        result = self.lib.is_book_by_author(tst_value_author, tst_value_book)
+        
+        self.lib.api.books_by_author.assert_called_once_with(tst_value_author)
+        self.assertFalse(result)
+
+    def test_languages_for_book_all(self):
+        self.lib.api.get_book_info = Mock(return_value=self.book_data)
+        tst_value_book = "Way of Kings"
+        
+        actual = self.lib.get_languages_for_book(tst_value_book)
+        expected = set(["por", "pol", "ger", "eng", "spa", "tst"])
+        
+        self.lib.api.get_book_info.assert_called_once_with(tst_value_book)
+        self.assertSetEqual(actual, expected)
+    
+    
